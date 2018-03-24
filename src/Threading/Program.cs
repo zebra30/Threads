3eacfdd2f2f681dc366ee1319f23d3bc4b930213
Alex Klo <aaom@nni.net>
2018-03-24 16:41:15 -0500

    Initial commit;

---
+++ src/Threading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            AnotherTask("ASDFASDF");
+        }
+
+        private void SpinUpTask()
+        {
+            Console.WriteLine("New  Task");
+        }
+
+        private void AnotherTask(string myString)
+        {
+            Task t = Task.Factory.StartNew((object lString) =>
+                    {
+                        Console.WriteLine(lString);
+                    }
+            , (String)myString);
+
+
+            t.Wait();
+
+          
+
+        }
+    }
+}
